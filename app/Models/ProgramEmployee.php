8549c12cc7c2f725e0d62ec0657f57146c49e516
OleGK4 <oleg.krut.64@gmail.com>
2023-11-20 11:11:37 +0700

    [added] ProgramEmployee model & it's relations

--- app/Models/ProgramEmployee.php
+++ app/Models/ProgramEmployee.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class ProgramEmployee extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'employee_id',
+        'program_id',
+    ];
+
+// Relations
+
+    public function program(): BelongsTo
+    {
+        return $this->belongsTo(Program::class);
+    }
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
